1d8f82a6a7f96a214fb83c455e269a94d2ae6c23
tfarnecim <36956453+tfarnecim@users.noreply.github.com>
2018-11-21 10:14:18 -0200

    Add files via upload

---
+++ Implementação.py
@@ -0,0 +1,14 @@
+def tira(lista):
+    return lista.pop()
+
+def coloca(lista,v):
+    return lista.append(v)
+
+def topo(lista):
+    return lista[len(lista)-1]
+
+def Vazia(lista):
+    if(len(lista) == 0):
+        return True
+    return False
+
